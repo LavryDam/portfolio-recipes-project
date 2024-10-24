1473a449b9974caf4b3f19fe6d8749230d6a2325
Dina <98164900+LavryDam@users.noreply.github.com>
2024-10-24 11:46:47 +0200

    fix:index

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-hamburguesa').addEventListener('click', function() {
+    const navegacion = document.querySelector('.navegacion');
+    navegacion.classList.toggle('mostrar');
+});
